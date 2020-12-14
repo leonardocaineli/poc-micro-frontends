aeadbfb24fef5a8636c0917d98855090e8c8dd4f
Leonardo Caineli <leonardo.caineli@kestraa.com.br>
2020-12-14 10:33:20 -0300

    app counter

--- app-counter/src/root.component.js
+++ app-counter/src/root.component.js
@@ -6,7 +6,7 @@ export default function Root(props) {
 	return (
 		<div className="app">
 			<h1>{props.name}</h1>
-			<h2>Counter: {counter}</h2>
+			<h2>{counter}</h2>
 			<button onClick={() => setCounter(counter - 1)}>Decrement</button>
 			<button onClick={() => setCounter(counter + 1)}>Increment</button>
 		</div>
